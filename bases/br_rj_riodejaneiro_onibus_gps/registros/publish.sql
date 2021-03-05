bd66c8f58622fa10cff75688aa596c91903c471a
Laura O. Moraes <laura.moraes@coppe.ufrj.br>
2021-03-05 20:22:22 -0300

    Properly creating onibus table config

--- bases/br_rj_riodejaneiro_onibus_gps/registros/publish.sql
+++ bases/br_rj_riodejaneiro_onibus_gps/registros/publish.sql
@@ -19,19 +19,15 @@ TIPOS:
 
 */
 
-CREATE VIEW rj-smtr.br_rj_riodejaneiro_brt_gps.registros AS
+CREATE VIEW rj-smtr-dev.br_rj_riodejaneiro_onibus_gps.registros AS
 SELECT 
-SAFE_CAST(codigo AS STRING) codigo,
-SAFE_CAST(placa AS STRING) placa,
-SAFE_CAST(linha AS STRING) linha,
-SAFE_CAST(latitude AS FLOAT64) latitude,
-SAFE_CAST(longitude AS FLOAT64) longitude,
+SAFE_CAST(ordem AS STRING) ordem,
+SAFE_CAST(latitude AS STRING) latitude,
+SAFE_CAST(longitude AS STRING) longitude,
 SAFE_CAST(DATETIME(TIMESTAMP(timestamp_gps), "America/Sao_Paulo") AS DATETIME) timestamp_gps,
-SAFE_CAST(velocidade AS INT64) velocidade,
-SAFE_CAST(id_migracao_trajeto AS STRING) id_migracao_trajeto,
-SAFE_CAST(sentido AS STRING) sentido,
-SAFE_CAST(trajeto AS STRING) trajeto,
+SAFE_CAST(velocidade AS STRING) velocidade,
+SAFE_CAST(linha AS STRING) linha,
 SAFE_CAST(DATETIME(TIMESTAMP(timestamp_captura), "America/Sao_Paulo") AS DATETIME) timestamp_captura,
 SAFE_CAST(data AS DATE) data,
 SAFE_CAST(hora AS INT64) hora
-from rj-smtr-staging.br_rj_riodejaneiro_brt_gps_staging.registros as t
+from rj-smtr-dev.br_rj_riodejaneiro_onibus_gps_staging.registros as t
