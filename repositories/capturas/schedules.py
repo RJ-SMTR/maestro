0b67d07a983c482c667542aa486dff332cbb9f33
Laura O. Moraes <laura.moraes@coppe.ufrj.br>
2021-03-05 22:25:04 -0300

    Onibus GPS schedule added

--- repositories/capturas/schedules.py
+++ repositories/capturas/schedules.py
@@ -17,3 +17,16 @@ def br_rj_riodejaneiro_brt_gps_registros(context):
     config["resources"]["timezone_config"]["config"]["timezone"] = timezone
     return config
 
+
+@schedule(
+    cron_schedule="* * * * *",
+    pipeline_name="br_rj_riodejaneiro_onibus_gps_registros",
+    name="br_rj_riodejaneiro_onibus_gps_registros",
+    mode="dev",
+    execution_timezone="America/Sao_Paulo"
+)
+def br_rj_riodejaneiro_onibus_gps_registros(context):
+    timezone = context.scheduled_execution_time.timezone.name
+    config = read_config(Path(__file__).parent / 'br_rj_riodejaneiro_onibus_gps/registros.yaml') 
+    config["resources"]["timezone_config"]["config"]["timezone"] = timezone
+    return config
