eb30e9514ac7553c01c56fffed712b54eab33994
Laura O. Moraes <laura.moraes@coppe.ufrj.br>
2021-05-12 23:23:18 -0300

    Bug-fix: realized trips publish.sql

--- bases/br_rj_riodejaneiro_gtfs_planned/realized_trips_planned/publish.sql
+++ bases/br_rj_riodejaneiro_gtfs_planned/realized_trips_planned/publish.sql
@@ -19,7 +19,7 @@ TIPOS:
 
 */
 
-CREATE VIEW rj-smtr-dev.br_rj_riodejaneiro_gtfs_planned.realized_trips_planned AS
+CREATE VIEW rj-smtr.br_rj_riodejaneiro_gtfs_planned.realized_trips_planned AS
 SELECT 
 SAFE_CAST(vehicle_id AS STRING) vehicle_id,
 SAFE_CAST(departure_datetime AS DATETIME) departure_datetime,
@@ -31,6 +31,6 @@ SAFE_CAST(elapsed_time AS FLOAT64) elapsed_time,
 SAFE_CAST(average_speed AS FLOAT64) average_speed,
 SAFE_CAST(trajectory_type AS STRING) trajectory_type,
 SAFE_CAST(trip_id AS STRING) trip_id,
-SAFE_CAST(trip_short_name AS STRING) trip_short_name
+SAFE_CAST(trip_short_name AS STRING) trip_short_name,
 SAFE_CAST(gtfs_version_date AS STRING) gtfs_version_date
-from rj-smtr-dev.br_rj_riodejaneiro_gtfs_planned_staging.realized_trips_planned as t
+from rj-smtr-staging.br_rj_riodejaneiro_gtfs_planned_staging.realized_trips_planned as t
