2b9125eb687c85aad8b1febb16694a203ac54f42
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-06-21 17:58:47 -0300

    Remove safe_cast queries

--- repositories/queries/views/br_rj_riodejaneiro_gtfs/shapes.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-SAFE_CAST(shape_id AS STRING) shape_id,
-SAFE_CAST(shape_pt_lat AS FLOAT64) shape_pt_lat,
-SAFE_CAST(shape_pt_lon AS FLOAT64) shape_pt_lon,
-SAFE_CAST(shape_pt_sequence AS INT64) shape_pt_sequence,
-SAFE_CAST(shape_dist_traveled AS FLOAT64) shape_dist_traveled,
-SAFE_CAST(DATETIME(PARSE_TIMESTAMP("%Y%m%d", CAST(gtfs_version_date AS STRING), "America/Sao_Paulo")) AS DATETIME) gtfs_version_date
-from rj-smtr-staging.br_rj_riodejaneiro_gtfs_staging.shapes as t
--- repositories/queries/views/br_rj_riodejaneiro_gtfs_planned/frequencies.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-SAFE_CAST(trip_id AS STRING) trip_id,
-SAFE_CAST(start_time AS TIME) start_time,
-SAFE_CAST(end_time AS TIME) end_time,
-SAFE_CAST(headway_secs AS INT64) headway_secs,
-SAFE_CAST(exact_times AS INT64) exact_times,
-SAFE_CAST(DATETIME(PARSE_TIMESTAMP("%Y%m%d", CAST(gtfs_version_date AS STRING), "America/Sao_Paulo")) AS DATETIME) gtfs_version_date
-from rj-smtr-staging.br_rj_riodejaneiro_gtfs_staging.frequencies as t
