c2b462a16e91dc213f29f4030309d429bae24ccb
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-06-26 09:47:09 -0300

    fix view names

--- repositories/queries/views/br_rj_riodejaneiro_brt_gps/registros_tratada_velocidade.sql
+++ repositories/queries/views/br_rj_riodejaneiro_brt_gps/registros_tratada_velocidade.sql
@@ -1,10 +1,19 @@
-WITH box AS (
-  SELECT
-    *
-  FROM `rj-smtr.br_rj_riodejaneiro_geo.limites_geograficos_caixa`
+WITH gps AS (
+  SELECT *, 
+    REGEXP_REPLACE(SPLIT(trajeto, ' ')[SAFE_OFFSET(0)], '[^a-zA-Z0-9]', '') linha_trajeto
+  FROM `rj-smtr.dashboard_monitoramento_brt.registros_filtrada` 
 )
-SELECT *
-FROM `rj-smtr.br_rj_riodejaneiro_brt_gps.registros` 
-WHERE DATETIME_DIFF(timestamp_captura, timestamp_gps, MINUTE) < 2
-AND longitude BETWEEN (SELECT min_longitude FROM box) AND (SELECT max_longitude FROM box)
-AND latitude BETWEEN (SELECT min_latitude FROM box) AND (SELECT max_latitude FROM box)
+SELECT 
+  t.*,
+  extract(time from t.timestamp_gps) as hora_completa,
+  linha_trajeto = linha flag_linha_similar_trajeto,
+  t2.velocidade as velocidade_estimada_10_min,
+  t2.nome_parada,
+  t2.tipo_parada,
+  t2.distancia_parada,
+  t2.status_movimento,
+  t2.status_tipo_parada
+FROM gps t
+JOIN `rj-smtr.dashboard_monitoramento_brt.velocidade_status` t2
+ON t.timestamp_captura = t2.timestamp_captura
+AND t.placa = t2.placa
