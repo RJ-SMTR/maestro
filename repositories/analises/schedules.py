2e0b9fc282d716a4ed65094f47f79557235be05e
hellcassius <caiorogerio.santos@gmail.com>
2021-06-28 14:13:54 -0300

    add new names to repository.yaml, add load yaml configs @schedules.py

--- repositories/analises/schedules.py
+++ repositories/analises/schedules.py
@@ -8,17 +8,24 @@ from repositories.helpers.helpers import read_config
     pipeline_name="br_rj_riodejaneiro_brt_gtfs_gps_realized_trips",
     start_date=datetime(2021, 1, 1),
     name="br_rj_riodejaneiro_brt_gtfs_gps",
-    execution_time=time(0, 30),
+    execution_time=time(17, 56),
     mode="dev",
     execution_timezone="America/Sao_Paulo",
 )
-def br_rj_riodejaneiro_brt_gtfs_gps(date):
-    return {
+def br_rj_riodejaneiro_brt_gtfs_gps_realized_trips(date):
+    config = read_config(
+        Path(__file__).parent / "br_rj_riodejaneiro_brt_gtfs_gps/realized_trips.yaml"
+    )
+    solids = [
+        "get_daily_brt_gps_data",
+        "download_gtfs_from_storage",
+        "update_realized_trips",
+    ]
+    solid_config = {
         "solids": {
-            "get_daily_brt_gps_data": {"config": {"date": date.strftime("%Y-%m-%d")}},
-            "download_gtfs_from_storage": {
-                "config": {"date": date.strftime("%Y-%m-%d")}
-            },
-            "update_realized_trips": {"config": {"date": date.strftime("%Y-%m-%d")}},
+            solid: {"config": {"date": date.strftime("%Y-%m-%d")}} for solid in solids
         }
     }
+    config.update(solid_config)
+
+    return config
