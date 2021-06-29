c2df8860724dd39ff70caf62d4fa9d8746c41ab1
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-06-29 10:49:17 -0300

    add sppo tratada v2

---
+++ repositories/queries/views/br_rj_riodejaneiro_onibus_gps/registros_tratada_v2.sql
@@ -0,0 +1,23 @@
+WITH garagem_polygon AS (
+  SELECT 
+    ST_GEOGFROMTEXT(WKT, make_valid => true) AS poly
+  FROM `rj-smtr.br_rj_riodejaneiro_geo.garagens_polygon` 
+),
+box AS (
+  SELECT
+    *
+  FROM `rj-smtr.br_rj_riodejaneiro_geo.limites_geograficos_caixa`
+),
+gps AS (
+  SELECT 
+    *,
+    ST_GEOGPOINT(longitude, latitude) ponto
+  FROM `rj-smtr.br_rj_riodejaneiro_onibus_gps.registros`
+  WHERE DATETIME_DIFF(timestamp_captura, timestamp_gps, MINUTE) < 2
+)
+SELECT
+  ordem, latitude, longitude, timestamp_gps, velocidade, linha, timestamp_captura, data, hora,
+  extract(time from gps.timestamp_captura) as hora_completa,
+  ST_INTERSECTS(ponto, (SELECT poly FROM garagem_polygon)) fora_garagem
+FROM gps
+WHERE ST_INTERSECTSBOX(ponto, (SELECT min_longitude FROM box), (SELECT min_latitude FROM box), (SELECT max_longitude FROM box), (SELECT max_latitude FROM box))
