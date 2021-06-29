6c17fad0193a360238eb0d5623aee291b8935f59
hellcassius <caiorogerio.santos@gmail.com>
2021-06-29 11:05:31 -0300

    schedule runs @00:30

--- repositories/analises/schedules.py
+++ repositories/analises/schedules.py
@@ -8,7 +8,7 @@ from repositories.helpers.helpers import read_config
     pipeline_name="br_rj_riodejaneiro_brt_gtfs_gps_realized_trips",
     start_date=datetime(2021, 1, 1),
     name="br_rj_riodejaneiro_brt_gtfs_gps",
-    execution_time=time(17, 56),
+    execution_time=time(0, 30),
     mode="dev",
     execution_timezone="America/Sao_Paulo",
 )
