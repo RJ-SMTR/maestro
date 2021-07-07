39ab28fd0de6aeb66ea412a7b5c3d60e88eabbfc
hellcassius <caiorogerio.santos@gmail.com>
2021-07-07 12:39:21 -0300

    clean unused hooks from hooks.py

--- repositories/helpers/hooks.py
+++ repositories/helpers/hooks.py
@@ -1,15 +1,9 @@
 from dagster import success_hook, failure_hook, HookContext
 import pendulum
 import requests
-import pandas as pd
-from basedosdados.table import Table
 from croniter import croniter
 from datetime import datetime
 from redis_pal import RedisPal
-from repositories.libraries.basedosdados.solids import (
-    append_to_bigquery_v2,
-    create_table_bq_v2,
-)
 
 
 @success_hook(required_resource_keys={"discord_webhook", "timezone_config"})
@@ -46,51 +40,3 @@ def redis_keepalive_on_failure(context: HookContext):
     url = context.resources.discord_webhook["url"]
     requests.post(url, data={"content": message})
 
-
-def upload_logs_to_bq(dataset_id, table_id, filepath, table_config):
-    tb = Table(table_id, dataset_id)
-    tb.create(
-        path=filepath,
-        if_table_exists="replace",
-        if_storage_data_exists="replace",
-        if_table_config_exists=table_config,
-    )
-    tb.publish(if_exists="pass")
-
-
-@success_hook(required_resource_keys={"basedosdados_config", "timezone_config"})
-def upload_success_to_BQ(context: HookContext):
-    if context.solid.name == "get_raw":
-        run_time = context.resources.timezone_config["run_datetime"]
-        df = pd.DataFrame(
-            {"timestamp_captura": [run_time], "sucesso": [True], "erro": [""]}
-        )
-        filepath = f"success_hook_{run_time}.csv"
-        df.to_csv(filepath, index=False)
-        upload_logs_to_bq(
-            dataset_id=context.resources.basedosdados_config["dataset_id"],
-            table_id=context.resources.basedosdados_config["table_id"] + "_logs",
-            filepath=filepath,
-            table_config="pass",
-        )
-
-
-@failure_hook(required_resource_keys={"basedosdados_config", "timezone_config"})
-def upload_failure_to_BQ(context: HookContext):
-    if context.solid.name == "get_raw":
-        run_time = context.resources.timezone_config["run_datetime"]
-        df = pd.DataFrame(
-            {
-                "timestamp_captura": [run_time],
-                "sucesso": [False],
-                "erro": [context.solid_exception],
-            }
-        )
-        filepath = f"failure_hook_{run_time}.csv"
-        df.to_csv(filepath, index=False)
-        upload_logs_to_bq(
-            dataset_id=context.resources.basedosdados_config["dataset_id"],
-            table_id=context.resources.basedosdados_config["table_id"] + "_logs",
-            filepath=filepath,
-            table_config="replace",
-        )
