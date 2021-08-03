385cbd24ee15566e87238bba3cab2873fde0327e
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-08-03 14:31:44 -0300

    unused files cleanup

--- repositories/queries/views/dashboard_monitoramento_sppo/agg_nveiculos_por_linha.sql
+++
@@ -1,5 +0,0 @@
-SELECT 
-  data, EXTRACT(HOUR FROM hora_completa) hora, linha, count(distinct ordem) n_veiculos
-FROM `rj-smtr.dashboard_monitoramento_sppo.registros_tratada_completa`
-where distancia_da_garagem_metros > 600
-GROUP BY data, EXTRACT(HOUR FROM hora_completa), linha
--- repositories/queries/views/dashboard_monitoramento_sppo/registros_tratada_completa_viacao_acari.sql
+++
@@ -1,5 +0,0 @@
-SELECT *
-FROM `rj-smtr.br_rj_riodejaneiro_onibus_gps.registros_tratada` 
-WHERE linha IN (
-'254', 'SN254', 'SP254', '277', 'SN277', '456', '457', 'SN457', 
-'607', 'SN607', '650', 'SV650', 'SN650', '667', '686')
