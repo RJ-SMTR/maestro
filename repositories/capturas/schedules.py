dcd170b5ddfb93c8b76af56d0b486ba4c4ccd030
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-08-23 19:03:58 -0300

    adiciona capturas para stpl (#112)
    * adiciona capturas para stpl
    
    * adiciona configs para base registros do stpl
    
    * fix safe cast

--- repositories/capturas/schedules.py
+++ repositories/capturas/schedules.py
@@ -6,6 +6,22 @@ from datetime import datetime, time
 
 @schedule(
     cron_schedule="* * * * *",
+    pipeline_name="br_rj_riodejaneiro_stpl_gps_registros",
+    name="br_rj_riodejaneiro_stpl_gps_registros",
+    mode="dev",
+    execution_timezone="America/Sao_Paulo",
+)
+def br_rj_riodejaneiro_stpl_gps_registros(context):
+    timezone = context.scheduled_execution_time.timezone.name
+    config = read_config(
+        Path(__file__).parent / "br_rj_riodejaneiro_stpl_gps/registros.yaml"
+    )
+    config["resources"]["timezone_config"]["config"]["timezone"] = timezone
+    return config
+
+
+@schedule(
+    cron_schedule="* * * * *",
     pipeline_name="br_rj_riodejaneiro_brt_gps_registros",
     name="br_rj_riodejaneiro_brt_gps_registros",
     mode="dev",
