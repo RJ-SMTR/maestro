505f51327c0cc44afe114e868696df1b4480d951
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-08-24 10:24:34 -0300

    dilui deletes do DB em 50min, roda pipeline de hora em hora

--- repositories/maintenance/schedules.py
+++ repositories/maintenance/schedules.py
@@ -1,23 +1,20 @@
 from pathlib import Path
 from datetime import datetime, time
 
-from dagster import daily_schedule, ScheduleExecutionContext
+from dagster import schedule, ScheduleExecutionContext
 from dagster.core.storage.event_log.schema import SqlEventLogStorageTable
 from dagster.core.storage.runs.schema import RunsTable, RunTagsTable, SnapshotsTable
 
 from repositories.helpers.helpers import read_config
 
 
-@daily_schedule(
-    pipeline_name='wipe_history',
-    start_date=datetime(2021, 1, 1),
-    name="wipe_history_daily",
-    execution_time=time(1, 30),
+@schedule(
+    cron_schedule="0 * * * *",
+    pipeline_name="wipe_history",
+    name="wipe_history_hourly",
     mode="dev",
     execution_timezone="America/Sao_Paulo",
 )
-def wipe_history_daily(context: ScheduleExecutionContext):
-    config = read_config(
-        Path(__file__).parent / "wipe_history.yaml"
-    )
+def wipe_history_hourly(context: ScheduleExecutionContext):
+    config = read_config(Path(__file__).parent / "wipe_history.yaml")
     return config
--- repositories/maintenance/solids.py
+++ repositories/maintenance/solids.py
@@ -1,59 +1,74 @@
+import time
 from datetime import datetime, timedelta
 
-from dagster import DagsterInstance
 from dagster import solid, SolidExecutionContext
 from dagster.core.definitions import InputDefinition
+from dagster.core.storage.pipeline_run import (
+    PipelineRunsFilter,
+    PipelineRunStatus,
+)
 
 from repositories.helpers.datetime import convert_unix_time_to_datetime
 
 
 @solid
-def get_dagster_instance(context) -> DagsterInstance:
-    return DagsterInstance.get()
+def get_runs(context: SolidExecutionContext):
+    return context.instance.get_runs(
+        filters=PipelineRunsFilter(
+            statuses=[
+                PipelineRunStatus.SUCCESS,
+                PipelineRunStatus.FAILURE,
+                PipelineRunStatus.CANCELED,
+            ]
+        )
+    )
 
 
 @solid
-def get_runs(context, instance: DagsterInstance):
-    return instance.get_runs()
-
-
-@solid
-def filter_runs(context, instance: DagsterInstance, runs, compare_timestamp: datetime):
-    filtered_runs = []
-    for run in runs:
-        run_stats = instance.get_run_stats(run.run_id)
+def filter_and_delete_runs(
+    context: SolidExecutionContext, runs, compare_timestamp: datetime
+):
+    instance = context.instance
+    total_runs = len(runs)
+    deleted_runs = 0
+    context.log.info(f"Collected {total_runs} runs")
+    tick_delay = 3000 / (total_runs + 1)  # Make it last 50 minutes
+    next_tick = time.time() + tick_delay
+    i = 0
+    while i < total_runs:
+        while time.time() < next_tick:
+            time.sleep(0.01)
+        run_stats = instance.get_run_stats(runs[i].run_id)
         if run_stats is None:
+            i += 1
+            next_tick += tick_delay
             continue
         launch_time = run_stats.launch_time
         if launch_time is None:
+            i += 1
+            next_tick += tick_delay
             continue
-        launch_time = convert_unix_time_to_datetime(
-            launch_time
-        )
+        launch_time = convert_unix_time_to_datetime(launch_time)
         if launch_time <= compare_timestamp:
-            filtered_runs.append(run)
-    return filtered_runs
-
-
-@solid
-def delete_runs(context, instance: DagsterInstance, runs):
-    context.log.info(f'Will delete {len(runs)} runs')
-    failed_runs = []
-    for run in runs:
-        try:
-            instance.delete_run(run.run_id)
-        except Exception as e:
-            failed_runs.append(run.run_id)
-    context.log.info(f'Deleted {len(runs) - len(failed_runs)} runs')
+            instance.delete_run(runs[i].run_id)
+            deleted_runs += 1
+        next_tick += tick_delay
+        i += 1
+    context.log.info(f"Deleted {deleted_runs} out of {total_runs} runs")
 
 
 @solid(
     input_defs=[
-        InputDefinition('seconds', int, default_value=0),
-        InputDefinition('minutes', int, default_value=0),
-        InputDefinition('hours', int, default_value=0),
-        InputDefinition('days', int, default_value=0),
+        InputDefinition("seconds", int, default_value=0),
+        InputDefinition("minutes", int, default_value=0),
+        InputDefinition("hours", int, default_value=0),
+        InputDefinition("days", int, default_value=0),
     ]
 )
-def get_compare_timestamp(context: SolidExecutionContext, seconds, minutes, hours, days) -> datetime:
-    return datetime.now() - timedelta(seconds=seconds, minutes=minutes, hours=hours, days=days)
+def get_compare_timestamp(
+    context: SolidExecutionContext, seconds, minutes, hours, days
+) -> datetime:
+    return datetime.now() - timedelta(
+        seconds=seconds, minutes=minutes, hours=hours, days=days
+    )
+
