ecab183a032cabd864d02a29e071370f4eaa0b57
hellcassius <caiorogerio.santos@gmail.com>
2021-09-30 16:04:33 -0300

    fix schedule loading, add logging

--- repositories/analises/projeto_multa_automatica/sumario_multa_onibus_integrado_stu.py
+++ repositories/analises/projeto_multa_automatica/sumario_multa_onibus_integrado_stu.py
@@ -15,14 +15,22 @@ from repositories.analises.resources import schedule_run_date
 )
 def query_data(context):
     project = context.resources.bd_client.project
+    context.log.info(
+        f"Fetching data from {project}.{context.solid_config['query_table']}"
+    )
     query = f"""
         SELECT *
         FROM {project}.{context.solid_config['query_table']}
     """
-    run_date = datetime.strptime(context.resources.schedule_run_date["date"]).strftime(
-        "%Y%m%d"
-    )
+    run_date = datetime.strptime(
+        context.resources.schedule_run_date["date"], "%Y-%m-%d"
+    ).strftime("%Y%m%d")
+
     filename = f"{run_date}/multas{run_date}.csv"
+
+    context.log.info(
+        f"Downloading query results and saving as {run_date}/multas{run_date}.csv"
+    )
     bd.download(
         savepath=filename, query=query, billing_project_id=project, index=False, sep=";"
     )
@@ -36,12 +44,21 @@ def upload(context, filename):
         dataset_id=context.solid_config["dataset_id"],
     )
     if not tb.table_exists("staging"):
+        context.log.info(
+            f"Table does not exist at STAGING, creating table {context.solid_config['dataset_id']}.{context.solid_config['table_id']}"
+        )
         tb.create(
             path=filename, if_table_config_exists="pass", if_storage_data_exists="pass"
         )
     elif not tb.table_exists("prod"):
+        context.log.info(
+            f"Table does not exist at PROD, creating view {context.solid_config['dataset_id']}.{context.solid_config['table_id']}"
+        )
         tb.publish()
     else:
+        context.log.info(
+            f"Table already exists, appending to table {context.solid_config['dataset_id']}.{context.solid_config['table_id']}"
+        )
         tb.append(filename)
 
     return filename
@@ -49,6 +66,7 @@ def upload(context, filename):
 
 @solid()
 def cleanup(context, filename):
+    context.log.info(f"Starting cleanup, deleting {filename} from local")
     return shutil.rmtree(filename)
 
 
@@ -75,8 +93,4 @@ def cleanup(context, filename):
     },
 )
 def projeto_multa_automatica_sumario_multa_onibus_integrado_stu():
-    cleanup(
-        upload(
-            query_data()
-        )
-    )
+    cleanup(upload(query_data()))
