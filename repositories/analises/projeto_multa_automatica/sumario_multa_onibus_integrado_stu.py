b073eaeaaeec6ee5085a9bd34899d6e8bcd81799
hellcassius <caiorogerio.santos@gmail.com>
2021-10-21 14:52:44 -0300

    fix issues with scheduling, functional for d-1

--- repositories/analises/projeto_multa_automatica/sumario_multa_onibus_integrado_stu.py
+++ repositories/analises/projeto_multa_automatica/sumario_multa_onibus_integrado_stu.py
@@ -1,11 +1,12 @@
 from os import replace
 import shutil
 import basedosdados as bd
+import basedosdados
 from dagster import solid, pipeline, ModeDefinition
 from basedosdados import Storage
 from pathlib import Path
 from datetime import datetime, timedelta
-from repositories.libraries.basedosdados.resources import bd_client
+from repositories.libraries.basedosdados.resources import bd_client, basedosdados_config
 from repositories.analises.resources import schedule_run_date
 
 
@@ -16,16 +17,23 @@ from repositories.analises.resources import schedule_run_date
 def query_data(context):
     project = context.resources.bd_client.project
     context.log.info(
+        f"""
+    ##### Solid Config:
+        query_table: {context.solid_config['query_table']}
+        date_format: {context.solid_config['date_format']}
+    #### Resources:
+        bd_client.project: {project}
+        schedule_run_date: {context.resources.schedule_run_date}
+    """
+    )
+    context.log.info(
         f"Fetching data from {project}.{context.solid_config['query_table']}"
     )
-    run_date = (
-        datetime.strptime(context.resources.schedule_run_date["date"], "%Y-%m-%d")
-        - timedelta(days=1)
-    ).strftime("%Y-%m-%d")
+    run_date = context.resources.schedule_run_date["date"]
 
     query = f"""
-        SELECT *
-        FROM {context.solid_config.query_table}
+        SELECT * except(data)
+        FROM {context.solid_config['query_table']}
         WHERE data = '{run_date}'
     """
     context.log.info(f"Running query\n {query}")
@@ -46,18 +54,17 @@ def query_data(context):
 
 
 @solid(
-    config_schema={"dataset_id": str, "table_id": str},
-    required_resource_keys={"bd_client"},
+    required_resource_keys={"bd_client", "basedosdados_config"},
 )
 def upload(context, filename):
     st = Storage(
-        dataset_id=context.solid_config["dataset_id"],
-        table_id=context.solid_config["table_id"],
+        dataset_id=context.resources.basedosdados_config["dataset_id"],
+        table_id=context.resources.basedosdados_config["table_id"],
     )
     context.log.info(
-        f"Uploading {filename} to GCS at {context.resources.bd_client.project}/staging/{context.solid_config['dataset_id']}/{context.solid_config['table_id']}"
+        f"Uploading {filename} to GCS at:{context.resources.bd_client.project}/staging/{context.resources.basedosdados_config['dataset_id']}/{context.resources.basedosdados_config['table_id']}",
     )
-    st.upload(path=filename, mode="staging", if_exists=replace)
+    st.upload(path=filename, mode="staging", if_exists="replace")
 
     return filename
 
@@ -74,6 +81,7 @@ def cleanup(context, filename):
             "dev",
             resource_defs={
                 "bd_client": bd_client,
+                "basedosdados_config": basedosdados_config,
                 "schedule_run_date": schedule_run_date,
             },
         )
