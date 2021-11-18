c11476a7e99a2a7417ee23e59e013e8c85150517
hellcassius <caiorogerio.santos@gmail.com>
2021-11-18 16:51:21 -0300

    fix bd+ config files wrong column order

--- bases/br_rj_riodejaneiro_brt_gps/registros/publish.sql
+++ bases/br_rj_riodejaneiro_brt_gps/registros/publish.sql
@@ -25,4 +25,6 @@ SAFE_CAST(id_veiculo AS STRING) id_veiculo,
 SAFE_CAST(DATETIME(TIMESTAMP(timestamp_gps), "America/Sao_Paulo" ) AS DATETIME) timestamp_gps,
 SAFE_CAST(DATETIME(TIMESTAMP_TRUNC(TIMESTAMP(timestamp_captura), SECOND), "America/Sao_Paulo" ) AS DATETIME) timestamp_captura,
 REPLACE(content,"None","") content,
+data,
+hora
 from rj-smtr-dev.br_rj_riodejaneiro_brt_gps_staging.registros as t
