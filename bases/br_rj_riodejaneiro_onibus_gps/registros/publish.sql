daa0858784ca4043cf69e83eea452f41d634f857
Caio R S dos Santos <caiorogerio.santos@gmail.com>
2021-11-25 14:36:26 -0300

    [SPPO] Conserta o publish.sql (#163)
    * change is init to false
    
    * change solid upload_to_bigquery to new bq_upload
    
    * add notification for solid failure
    
    * fix publish.sql
    
    Co-authored-by: Fernanda Scovino <fscovinom@gmail.com>

--- bases/br_rj_riodejaneiro_onibus_gps/registros/publish.sql
+++ bases/br_rj_riodejaneiro_onibus_gps/registros/publish.sql
@@ -24,7 +24,7 @@ SELECT
 SAFE_CAST(ordem AS STRING) ordem,
 SAFE_CAST(REPLACE(latitude, ',', '.') AS FLOAT64) latitude,
 SAFE_CAST(REPLACE(longitude, ',', '.') AS FLOAT64) longitude,
-SAFE_CAST(DATETIME(TIMESTAMP(timestamp_gps), "America/Sao_Paulo") AS DATETIME) timestamp_gps,
+SAFE_CAST(DATETIME(TIMESTAMP(datahora), "America/Sao_Paulo") AS DATETIME) timestamp_gps,
 SAFE_CAST(velocidade AS INT64) velocidade,
 SAFE_CAST(linha AS STRING) linha,
 SAFE_CAST(DATETIME(TIMESTAMP(timestamp_captura), "America/Sao_Paulo") AS DATETIME) timestamp_captura,
